efeee49144f2c934bd4012ad6ef9489b998eca9e
Lucas Lima <89950016+LucasChagasLima@users.noreply.github.com>
2022-10-14 15:27:06 -0300

    C#

---
+++ C#/crescente/crescente/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace crescente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            int x, y;
+
+            Console.WriteLine("Digite dois numeros: ");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            while(x != y)
+            {
+                if(x > y)
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                else
+                {
+                    Console.WriteLine("Crescente");
+                }
+                Console.WriteLine("Digite outros 2 numeros: ");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
