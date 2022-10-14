d7245728a5f6125f4bb73fda253d95001a932a7c
Lucas Lima <89950016+LucasChagasLima@users.noreply.github.com>
2022-10-14 15:29:18 -0300

    C# 2

---
+++ C#/diagonal_principal/diagonal_principal/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+namespace diagonal_principal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            int i, n, j, negativos;
+
+            Console.Write("Qual a ordem da matriz? ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write("Elemento [" + i + " ," + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+
+                }
+
+                
+            }
+            Console.WriteLine("Diagonal principal: ");
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(" " + mat[i, j]);
+                    }
+                }
+            }
+            Console.WriteLine(" ");
+            negativos = 0;
+
+            for(i = 0; i < n; i++)
+            {
+                for(j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        negativos++;
+                    }
+                }
+            }
+            Console.WriteLine("Quantidade de negativos = " + negativos);
+        }
+    }
+}
---
+++ C#/idades/idades/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace idades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            String nome1, nome2;
+            int idade1, idade2;
+            double media1;
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Nome: ");
+            nome1 = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dados da sugunda pessoa: ");
+            Console.Write("Nome: ");
+            nome2 = Console.ReadLine();
+            Console.Write("Idade :");
+            idade2 = int.Parse(Console.ReadLine());
+
+            media1 = (double) (idade1 + idade2) / 2;
+
+            Console.WriteLine("A idade media de " + nome1 + " e " + nome2 + " e de " + media1.ToString("F1", ci) + " anos");
+
+
+        }
+    }
+}
